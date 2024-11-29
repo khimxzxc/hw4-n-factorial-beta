25187b494e1b0bb92de41652c272df5d979aa5d2
khimxzxc <127808298+khimxzxc@users.noreply.github.com>
2024-11-29 23:32:37 +0500

    Add files via upload

--- HW4/app.js
+++ HW4/app.js
@@ -48,3 +48,37 @@ alert(pow(x, n));
 */
 
 //3. Перепишите с использованием функции-стрелки
+/** 
+
+let ask = (question,yes,no) => {
+  if (confirm(question)) yes()
+    else no();
+}
+
+ask("Вы согласны?",
+    () => alert("Вы согласились."),
+    () => alert("Вы отменили выполнение.")
+);
+*/
+
+//4. Сортировать в порядке по убыванию
+/** 
+let arr = [5, 2, 1, -10, 8];
+
+arr.sort((a, b) => b - a);
+
+alert( arr ); // 8, 5, 2, 1, -10
+
+*/
+
+//5. Трансформировать в массив имён
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+let names = users.map
+
+alert( names ); // Вася, Петя, Маша
